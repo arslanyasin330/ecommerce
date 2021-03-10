8abd37f1d037595b9c0a65e3ab9aad9fb670d5e8
nsaleem <muhammad.a@arbisoft.com>
2021-03-10 11:10:05 +0500

    login logout registration

---
+++ account/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('register/', views.register, name='register'),
+
+]
